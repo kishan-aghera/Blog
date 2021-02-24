dcc0a8b1b22b9b7a5f5cab353bb2849248b90677
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-02-24 17:21:10 +0530

    Added functionality for comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :update, :edit, :destroy]
   
   def index
     @posts = Post.all.order("created_at DESC")
@@ -9,7 +10,6 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.new(post_params)
 
     if @post.save
       redirect_to @post
@@ -19,11 +19,9 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
 
     if @post.update(post_params)
       redirect_to @post
@@ -33,11 +31,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
 
     redirect_to posts_path
@@ -49,5 +45,9 @@ class PostsController < ApplicationController
       :title,
       :content
     )
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
